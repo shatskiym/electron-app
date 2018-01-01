4b1fce138a44a008a5074e38aa6a7a0855d4a815
shatskiym <michael.shatskiy@gmail.com>
2018-01-01 17:44:34 +0200

    Cleared code

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 let imagesArray;
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('DOM LOADED!!!');
   imagesArray = [
     'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/220px-Image_created_with_a_mobile_phone.png',
     'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/Faust_bei_der_Arbeit.JPG/1024px-Faust_bei_der_Arbeit.JPG',
