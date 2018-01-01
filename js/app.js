d57791e54225c0e5af480f750bee83bfac620c5f
shatskiym <michael.shatskiy@gmail.com>
2018-01-01 17:35:51 +0200

    Added hot reload

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 let imagesArray;
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('DOM LOADED');
+  console.log('DOM LOADED!!!');
   imagesArray = [
     'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/220px-Image_created_with_a_mobile_phone.png',
     'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/Faust_bei_der_Arbeit.JPG/1024px-Faust_bei_der_Arbeit.JPG',
